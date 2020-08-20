c5e907c1bcc5eda0f276c523621c5db61756864f
James Cameron <jdonaldcameron@gmail.com>
2020-08-20 19:24:26 +1000

    feat: separate spotify api calls from redux actions

--- src/spotify/index.js
+++ src/spotify/index.js
@@ -7,6 +7,135 @@ const headers = {
   'Content-Type': 'application/json',
 };
 
+/**
+ * Get the authorized users profile
+ */
+export const getUserProfile = async () => {
+  const { data } = await axios.get('https://api.spotify.com/v1/me', {
+    headers,
+  });
+  return data;
+};
+
+/**
+ * Get the authorized users all time top artists
+ */
+export const getAllTimeArtists = async () => {
+  const {
+    data: { items },
+  } = await axios.get(
+    'https://api.spotify.com/v1/me/top/artists?limit=50&time_range=long_term',
+    {
+      headers,
+    }
+  );
+  return items;
+};
+
+/**
+ * Get the authorized users top artists from the past six months
+ */
+export const getSixMonthArtists = async () => {
+  const {
+    data: { items },
+  } = await axios.get(
+    'https://api.spotify.com/v1/me/top/artists?limit=50&time_range=medium_term',
+    {
+      headers,
+    }
+  );
+  return items;
+};
+
+/**
+ * Get the authorized users top artists from the past month
+ */
+export const getMonthArtists = async () => {
+  const {
+    data: { items },
+  } = await axios.get(
+    'https://api.spotify.com/v1/me/top/artists?limit=50&time_range=short_term',
+    {
+      headers,
+    }
+  );
+  return items;
+};
+
+/**
+ * Get the authorized users all time top tracks
+ */
+export const getAllTimeTracks = async () => {
+  const {
+    data: { items },
+  } = await axios.get(
+    'https://api.spotify.com/v1/me/top/tracks?limit=50&time_range=long_term',
+    {
+      headers,
+    }
+  );
+  return items;
+};
+
+/**
+ * Get the authorized users top tracks from the past six months
+ */
+export const getSixMonthsTracks = async () => {
+  const { data: { items } } = await axios.get(
+    'https://api.spotify.com/v1/me/top/tracks?limit=50&time_range=medium_term',
+    {
+      headers,
+    }
+  );
+  return items;
+};
+
+/**
+ * Get the authorized users top tracks from the past month
+ */
+export const getMonthTracks = async () => {
+  const { data: { items } } = await axios.get(
+    'https://api.spotify.com/v1/me/top/tracks?limit=50&time_range=short_term',
+    {
+      headers,
+    }
+  );
+  return items;
+};
+
+/**
+ * Get the authorized users recently played tracks
+ */
+export const getRecentlyPlayed = async () => {
+  const {
+    data: { items },
+  } = await axios.get(
+    'https://api.spotify.com/v1/me/player/recently-played?limit=50',
+    {
+      headers,
+    }
+  );
+  return items;
+};
+
+/**
+ * Get a track object
+ * @param {String} trackId
+ */
+export const getTrack = async (trackId) => {
+  const { data } = await axios.get(
+    `https://api.spotify.com/v1/tracks/${trackId}`,
+    {
+      headers,
+    }
+  );
+  return data;
+};
+
+/**
+ * Get the features of a track (loudness, liveness, etc)
+ * @param {String} trackId
+ */
 export const getTrackFeatures = async (trackId) => {
   const { data } = await axios.get(
     `https://api.spotify.com/v1/audio-features/${trackId}`,
@@ -17,6 +146,10 @@ export const getTrackFeatures = async (trackId) => {
   return data;
 };
 
+/**
+ * Get the audio analysis of a track
+ * @param {String} trackId
+ */
 export const getTrackAnalysis = async (trackId) => {
   const { data } = await axios.get(
     `https://api.spotify.com/v1/audio-analysis/${trackId}`,
@@ -27,12 +160,86 @@ export const getTrackAnalysis = async (trackId) => {
   return data;
 };
 
+/**
+ * Get tracks similar to the id supplied
+ * @param {String} trackId
+ */
 export const getTrackRecommendations = async (trackId) => {
   const {
     data,
   } = await axios.get(
     `https://api.spotify.com/v1/recommendations?seed_tracks=${trackId}&limit=16`,
     { headers }
+  );
+  return data;
+};
+
+/**
+ * Get an album object
+ * @param {String} albumId
+ */
+export const getAlbum = async (albumId) => {
+  const { data } = await axios.get(
+    `https://api.spotify.com/v1/albums/${albumId}`,
+    {
+      headers,
+    }
+  );
+  return data;
+};
+
+/**
+ * Get an artist object
+ * @param {String} artistId
+ */
+export const getArtist = async (artistId) => {
+  const { data } = await axios.get(
+    `https://api.spotify.com/v1/artists/${artistId}`,
+    {
+      headers,
+    }
+  );
+  return data;
+};
+
+/**
+ * Get an artists albums and singles
+ * @param {String} artistId
+ */
+export const getArtistAlbums = async (artistId) => {
+  const { data } = await axios.get(
+    `https://api.spotify.com/v1/artists/${artistId}/albums?include_groups=album,single`,
+    {
+      headers,
+    }
+  );
+  return data;
+};
+
+/**
+ * Get an artists top tracks
+ * @param {String} artistId
+ */
+export const getArtistTopTracks = async (artistId) => {
+  const { data } = await axios.get(
+    `https://api.spotify.com/v1/artists/${artistId}/top-tracks`,
+    {
+      headers,
+    }
+  );
+  return data;
+};
+
+/**
+ * Get artists related to the supplied artist
+ * @param {String} artistId
+ */
+export const getRelatedArtist = async (artistId) => {
+  const { data } = await axios.get(
+    `https://api.spotify.com/v1/artists/${artistId}/related-artists`,
+    {
+      headers,
+    }
   );
   return data;
 };
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,19 +1,10 @@
-import axios from 'axios';
-
 import * as types from './types';
-import { token } from '../utils/tokenHelpers';
-
-const headers = {
-  Authorization: `Bearer ${token}`,
-  'Content-Type': 'application/json',
-};
+import * as spotify from '../spotify';
 
 export const getUserProfile = () => async (dispatch) => {
   try {
     dispatch({ type: types.PROFILE_LOADING_STATE, state: true });
-    const { data } = await axios.get('https://api.spotify.com/v1/me', {
-      headers,
-    });
+    const data = await spotify.getUserProfile();
     dispatch({ type: types.GET_USER_PROFILE_SUCCESS, profile: data });
   } catch (error) {
     await dispatch({ type: types.GET_USER_PROFILE_ERROR });
@@ -24,15 +15,10 @@ export const getUserProfile = () => async (dispatch) => {
 export const getAllTimeArtists = () => async (dispatch) => {
   try {
     dispatch({ type: types.TOP_ARTISTS_LOADING_STATE, state: true });
-    const { data } = await axios.get(
-      'https://api.spotify.com/v1/me/top/artists?limit=50&time_range=long_term',
-      {
-        headers,
-      }
-    );
+    const artists = await spotify.getAllTimeArtists();
     dispatch({
       type: types.GET_TOP_ARTISTS_ALL_TIME_SUCCESS,
-      artists: data.items,
+      artists,
     });
   } catch (error) {
     await dispatch({ type: types.GET_TOP_ARTISTS_ALL_TIME_ERROR });
@@ -43,15 +29,10 @@ export const getAllTimeArtists = () => async (dispatch) => {
 export const getHalfYearArtists = () => async (dispatch) => {
   try {
     dispatch({ type: types.TOP_ARTISTS_LOADING_STATE, state: true });
-    const { data } = await axios.get(
-      'https://api.spotify.com/v1/me/top/artists?limit=50&time_range=medium_term',
-      {
-        headers,
-      }
-    );
+    const artists = await spotify.getSixMonthArtists();
     dispatch({
       type: types.GET_TOP_ARTISTS_HALF_YEAR_SUCCESS,
-      artists: data.items,
+      artists,
     });
   } catch (error) {
     await dispatch({ type: types.GET_TOP_ARTISTS_HALF_YEAR_ERROR });
@@ -62,15 +43,10 @@ export const getHalfYearArtists = () => async (dispatch) => {
 export const getMonthArtists = () => async (dispatch) => {
   try {
     dispatch({ type: types.TOP_ARTISTS_LOADING_STATE, state: true });
-    const { data } = await axios.get(
-      'https://api.spotify.com/v1/me/top/artists?limit=50&time_range=short_term',
-      {
-        headers,
-      }
-    );
+    const artists = await spotify.getMonthArtists();
     dispatch({
       type: types.GET_TOP_ARTISTS_MONTH_SUCCESS,
-      artists: data.items,
+      artists,
     });
   } catch (error) {
     await dispatch({ type: types.GET_TOP_ARTISTS_MONTH_ERROR });
@@ -81,15 +57,10 @@ export const getMonthArtists = () => async (dispatch) => {
 export const getAllTimeTracks = () => async (dispatch) => {
   try {
     dispatch({ type: types.TOP_TRACKS_LOADING_STATE, state: true });
-    const { data } = await axios.get(
-      'https://api.spotify.com/v1/me/top/tracks?limit=50&time_range=long_term',
-      {
-        headers,
-      }
-    );
+    const tracks = await spotify.getAllTimeTracks();
     dispatch({
       type: types.GET_TOP_TRACKS_ALL_TIME_SUCCESS,
-      tracks: data.items,
+      tracks,
     });
   } catch (error) {
     await dispatch({ type: types.GET_TOP_TRACKS_ALL_TIME_ERROR });
@@ -100,15 +71,10 @@ export const getAllTimeTracks = () => async (dispatch) => {
 export const getHalfYearTracks = () => async (dispatch) => {
   try {
     dispatch({ type: types.TOP_TRACKS_LOADING_STATE, state: true });
-    const { data } = await axios.get(
-      'https://api.spotify.com/v1/me/top/tracks?limit=50&time_range=medium_term',
-      {
-        headers,
-      }
-    );
+    const tracks = await spotify.getSixMonthsTracks();
     dispatch({
       type: types.GET_TOP_TRACKS_HALF_YEAR_SUCCESS,
-      tracks: data.items,
+      tracks,
     });
   } catch (error) {
     await dispatch({ type: types.GET_TOP_TRACKS_HALF_YEAR_ERROR });
@@ -119,15 +85,10 @@ export const getHalfYearTracks = () => async (dispatch) => {
 export const getMonthTracks = () => async (dispatch) => {
   try {
     dispatch({ type: types.TOP_TRACKS_LOADING_STATE, state: true });
-    const { data } = await axios.get(
-      'https://api.spotify.com/v1/me/top/tracks?limit=50&time_range=short_term',
-      {
-        headers,
-      }
-    );
+    const tracks = await spotify.getMonthTracks();
     dispatch({
       type: types.GET_TOP_TRACKS_MONTH_SUCCESS,
-      tracks: data.items,
+      tracks,
     });
   } catch (error) {
     await dispatch({ type: types.GET_TOP_TRACKS_MONTH_ERROR });
@@ -138,17 +99,10 @@ export const getMonthTracks = () => async (dispatch) => {
 export const getRecentlyPlayed = () => async (dispatch) => {
   try {
     dispatch({ type: types.RECENTLY_PLAYED_LOADING_STATE, state: true });
-    const {
-      data: { items },
-    } = await axios.get(
-      'https://api.spotify.com/v1/me/player/recently-played?limit=50',
-      {
-        headers,
-      }
-    );
+    const recentlyPlayed = await spotify.getRecentlyPlayed();
     dispatch({
       type: types.GET_RECENTLY_PLAYED_SUCCESS,
-      recentlyPlayed: items,
+      recentlyPlayed,
     });
   } catch (error) {
     await dispatch({ type: types.GET_RECENTLY_PLAYED_ERROR });
@@ -159,15 +113,10 @@ export const getRecentlyPlayed = () => async (dispatch) => {
 export const getTrack = (trackId) => async (dispatch) => {
   try {
     dispatch({ type: types.TRACK_LOADING_STATE, state: true });
-    const { data } = await axios.get(
-      `https://api.spotify.com/v1/tracks/${trackId}`,
-      {
-        headers,
-      }
-    );
+    const track = await spotify.getTrack(trackId);
     dispatch({
       type: types.GET_TRACK_SUCCESS,
-      track: data,
+      track,
     });
     dispatch({ type: types.TRACK_LOADING_STATE, state: false });
   } catch (error) {
