f8f1ef8bb37070cbf4f2f201e490f45a9e358c00
James Cameron <jdonaldcameron@gmail.com>
2020-08-30 21:26:15 +1000

    feat: add feature artist links to album tracklist

--- src/components/AlbumTracks.js
+++ src/components/AlbumTracks.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
 
+import mixins from '../styles/mixins';
+import { formatDuration } from '../utils/helpers';
+
 const List = styled.ul`
   list-style: none;
   padding: 0;
@@ -10,38 +13,106 @@ const List = styled.ul`
 `;
 
 const ListItem = styled.li`
+  display: flex;
+  align-items: center;
   cursor: pointer;
   border-bottom: 1px solid ${({ theme }) => theme.secondary};
-  display: flex;
   min-height: 0px;
+  padding: 8px;
   &:hover {
     border-bottom: 1px solid ${({ theme }) => theme.tertiary};
   }
 `;
 
 const TrackName = styled.h3`
+  font-size: 14px;
+  text-transform: uppercase;
   color: ${({ theme }) => theme.text};
   margin: 0px;
+  flex: 1;
+  width: 0px;
+  ${mixins.overflowEllipsis}
 `;
 
-const AlbumTracks = ({ tracks }) => {
+const FeatureArtist = styled.span`
+  text-transform: initial;
+  font-weight: 300;
+  &:hover {
+    color: ${({ theme }) => theme.main};
+    text-decoration: underline;
+  }
+`;
+
+const Duration = styled.span`
+  display: flex;
+  align-items: center;
+  font-size: 12px;
+`;
+
+const AlbumTracks = ({ tracks, albumArtists }) => {
+  // Hooks
   const history = useHistory();
 
-  const onTrackClick = (trackId) => history.push(`/track/${trackId}`);
+  // Event handlers
+  const onTrackClick = (trackId) => {
+    history.push(`/track/${trackId}`);
+  };
+
+  const onArtistClick = (e, artistId) => {
+    e.stopPropagation();
+    history.push(`/artist/${artistId}`);
+  };
 
   return (
     <List>
-      {tracks.map((track) => (
-        <ListItem onClick={() => onTrackClick(track.id)} role="presentation">
-          <TrackName>{track.name}</TrackName>
-        </ListItem>
-      ))}
+      {tracks.map((track) => {
+        const featuredArtists = track.artists.filter(
+          (artist) => !albumArtists.some((a) => a.id === artist.id)
+        );
+
+        return (
+          <ListItem
+            key={track.id}
+            onClick={(e) => {
+              e.stopPropagation();
+              onTrackClick(track.id);
+            }}
+            role="presentation"
+          >
+            <TrackName>
+              {track.name}
+              {featuredArtists.length > 0 && (
+                <>
+                  <span> - </span>
+                  {featuredArtists.map(
+                    ({ id: artistId, name: artistName }, idx) => (
+                      <>
+                        <FeatureArtist
+                          onClick={(e) => onArtistClick(e, artistId)}
+                        >
+                          {artistName}
+                        </FeatureArtist>
+                        {featuredArtists.length > 0 &&
+                        idx === featuredArtists.length - 1
+                          ? ''
+                          : ', '}
+                      </>
+                    )
+                  )}
+                </>
+              )}
+            </TrackName>
+            <Duration>{formatDuration(track.duration_ms)}</Duration>
+          </ListItem>
+        );
+      })}
     </List>
   );
 };
 
 AlbumTracks.propTypes = {
   tracks: PropTypes.array,
+  albumArtists: PropTypes.array,
 };
 
 AlbumTracks.defaultProps = {
